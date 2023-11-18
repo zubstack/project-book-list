7c87f954aafbba380c00ee63d291d9b69fa1cec2
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2023-11-18 15:30:52 -0500

    [up] Render books from local db

---
+++ temp_db.js
@@ -0,0 +1,44 @@
+const books = [
+  {
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    recommendation: 8,
+    review:
+      "Fitzgerald's prose is as dazzling as the parties he describes. A compelling narrative of love, wealth, and disillusionment.Harper Lee's masterpiece is a powerful narrative that addresses profound societal issues with grace and humanity.Harper Lee's masterpiece is a powerful narrative that addresses profound societal issues with grace and humanity.Harper Lee's masterpiece is a powerful narrative that addresses profound societal issues with grace and humanity.Harper Lee's masterpiece is a powerful narrative that addresses profound societal issues with grace and humanity.",
+    ISBN: "9780743273565",
+  },
+  {
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    recommendation: 8,
+    review:
+      "Harper Lee's masterpiece is a powerful narrative that addresses profound societal issues with grace and humanity.Harper Lee's masterpiece is a powerful narrative that addresses profound societal issues with grace and humanity.Harper Lee's masterpiece is a powerful narrative that addresses profound societal issues with grace and humanity.Harper Lee's masterpiece is a powerful narrative that addresses profound societal issues with grace and humanity.Harper Lee's masterpiece is a powerful narrative that addresses profound societal issues with grace and humanity.",
+    ISBN: "9780061120084",
+  },
+  {
+    title: "1984",
+    author: "George Orwell",
+    recommendation: 8,
+    review:
+      "Orwell's chilling vision of a surveillance state remains relevant, serving as a stark reminder of the importance of safeguarding liberty.Harper Lee's masterpiece is a powerful narrative that addresses profound societal issues with grace and humanity.Harper Lee's masterpiece is a powerful narrative that addresses profound societal issues with grace and humanity.Harper Lee's masterpiece is a powerful narrative that addresses profound societal issues with grace and humanity.",
+    ISBN: "9780451524935",
+  },
+  {
+    title: "The Hitchhiker's Guide to the Galaxy",
+    author: "Douglas Adams",
+    recommendation: 8,
+    review:
+      "Adams' wit and humor shine in this intergalactic romp, making it a must-read for anyone in need of a good laugh and a dash of cosmic insight.Harper Lee's masterpiece is a powerful narrative that addresses profound societal issues with grace and humanity.Harper Lee's masterpiece is a powerful narrative that addresses profound societal issues with grace and humanity.Harper Lee's masterpiece is a powerful narrative that addresses profound societal issues with grace and humanity.Harper Lee's masterpiece is a powerful narrative that addresses profound societal issues with grace and humanity.",
+    ISBN: "9780345391803",
+  },
+  {
+    title: "The Catcher in the Rye",
+    author: "J.D. Salinger",
+    recommendation: 8,
+    review:
+      "Salinger captures the angst and confusion of youth with unparalleled authenticity. A timeless exploration of teenage rebellion and self-discovery.Harper Lee's masterpiece is a powerful narrative that addresses profound societal issues with grace and humanity.Harper Lee's masterpiece is a powerful narrative that addresses profound societal issues with grace and humanity.Harper Lee's masterpiece is a powerful narrative that addresses profound societal issues with grace and humanity.Harper Lee's masterpiece is a powerful narrative that addresses profound societal issues with grace and humanity.",
+    ISBN: "9780061120084",
+  },
+];
+
+module.exports = books;
